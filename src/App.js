a27df4d9fa0a215e4b98ef04f1a9ba73d124a7db
João Harbs <harbsprog@gmail.com>
2020-04-09 21:00:38 -0300

    Add and list repositories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import api from 'services/api'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then((response) => {
+      setRepositories(response.data)
+    })
+  }, [])
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Plug2Hub',
+      url: 'https://www.github.com/harbsprog/plug2hub',
+      tech: ['NodeJS', 'ReactJS', 'React Native', 'Laravel'],
+    })
+
+    setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
@@ -14,18 +28,17 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
